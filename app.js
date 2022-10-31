30734b734313fa4349b50cfefb0fdd57389e266b
Alejae1998 <alejagr1998@gmail.com>
2022-10-31 15:09:16 -0700

    fixing the reset function

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 /* Imports */
 const button1 = document.getElementById('button-1');
+const button2 = document.getElementById('button-2');
+const button3 = document.getElementById('button-3');
+const resetButton = document.querySelector('#reset');
 const img1 = document.querySelector('#img-1');
 const img2 = document.querySelector('#img-2');
 const img3 = document.querySelector('#img-3');
-const resetButton = document.querySelector('#reset');
 
 const winsSpan = document.getElementById('wins');
 const lossesSpan = document.getElementById('losses');
@@ -27,6 +29,34 @@ button1.addEventListener('click', () => {
     }
     displayResults();
 });
+button2.addEventListener('click', () => {
+    total++;
+    resetPearls();
+    const randomPearl = Math.ceil(Math.random() * 3);
+    if (randomPearl === 1) {
+        img1.classList.add('reveal');
+    } else if (randomPearl === 2) {
+        wins++;
+        img2.classList.add('reveal');
+    } else {
+        img3.classList.add('reveal');
+    }
+    displayResults();
+});
+button3.addEventListener('click', () => {
+    total++;
+    resetPearls();
+    const randomPearl = Math.ceil(Math.random() * 3);
+    if (randomPearl === 1) {
+        img1.classList.add('reveal');
+    } else if (randomPearl === 2) {
+        img2.classList.add('reveal');
+    } else {
+        wins++;
+        img3.classList.add('reveal');
+    }
+    displayResults();
+});
 
 //reset button
 resetButton.addEventListener('click', () => {
@@ -39,8 +69,8 @@ resetButton.addEventListener('click', () => {
 /* Display Functions */
 function resetPearls() {
     img1.classList.remove('reveal');
-    img1.classList.remove('reveal');
-    img1.classList.remove('reveal');
+    img2.classList.remove('reveal');
+    img3.classList.remove('reveal');
 }
 
 function displayResults() {
